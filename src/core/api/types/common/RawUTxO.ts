c173fe58ad020382591544c261eddc34c2b2faaf
rsalakhov <r.salahov@spectrumlabs.fi>
2024-04-16 11:16:13 +0300

    fill raw utxo type

--- src/core/api/types/common/RawUTxO.ts
+++ src/core/api/types/common/RawUTxO.ts
@@ -1,1 +1,29 @@
-export interface RawUTxO {}
+import {
+  Bech32String,
+  BlockHash,
+  CborHexString,
+  HexString,
+  OutputReferenceHash,
+  TransactionHash,
+  uint,
+} from '../../../types/types.ts';
+
+export interface RawUTxOCurrency {
+  readonly policyId: HexString;
+  readonly name: HexString;
+  readonly quantity: number;
+  readonly jsQuantity: string;
+}
+
+export interface RawUTxO {
+  readonly ref: OutputReferenceHash;
+  readonly blockHash: BlockHash;
+  readonly txHash: TransactionHash;
+  readonly index: uint;
+  readonly globalIndex: uint;
+  readonly addr: Bech32String;
+  readonly paymentCred: HexString;
+  readonly value: RawUTxOCurrency[];
+  readonly dataBin: CborHexString;
+  readonly spentByTxHash: TransactionHash;
+}
