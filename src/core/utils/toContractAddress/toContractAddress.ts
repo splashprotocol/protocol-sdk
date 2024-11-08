3480b0acac1fd87e083d8d64b7cd07d7e7d54e5e
ridel1e <ridellee@gmail.com>
2024-11-08 18:46:46 +0300

    updated to contract address fn

--- src/core/utils/toContractAddress/toContractAddress.ts
+++ src/core/utils/toContractAddress/toContractAddress.ts
@@ -23,14 +23,14 @@ export const toContractAddress = (
 ): Bech32String =>
   stakeKeyHash
     ? BaseAddress.new(
-        Number(network),
+        Number(network.network()),
         Credential.new_script(ScriptHash.from_hex(script)),
         Credential.new_pub_key(Ed25519KeyHash.from_hex(stakeKeyHash)),
       )
         .to_address()
         .to_bech32()
     : EnterpriseAddress.new(
-        Number(network),
+        Number(network.network()),
         Credential.new_script(ScriptHash.from_hex(script)),
       )
         .to_address()
